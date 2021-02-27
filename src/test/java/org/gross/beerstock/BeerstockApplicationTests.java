431327e4431351a1d1d66d7da052374049aec41c
Ádler Jonas Gross <adlerjgross@gmail.com>
2021-02-27 02:34:03 -0300

    First commit
    Sample classes from spring initializr

---
+++ src/test/java/org/gross/beerstock/BeerstockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gross.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
