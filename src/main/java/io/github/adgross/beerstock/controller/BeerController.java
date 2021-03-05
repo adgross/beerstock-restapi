48f4edfab41a1f73c25bb40cb43e2d56f629106f
Ádler Jonas Gross <adlerjgross@gmail.com>
2021-03-05 17:56:25 -0300

    API createBeer: add validation

--- src/main/java/io/github/adgross/beerstock/controller/BeerController.java
+++ src/main/java/io/github/adgross/beerstock/controller/BeerController.java
@@ -5,6 +5,7 @@ import io.github.adgross.beerstock.exception.BeerAlreadyRegisteredException;
 import io.github.adgross.beerstock.exception.BeerNotFoundException;
 import io.github.adgross.beerstock.services.BeerService;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +32,8 @@ public class BeerController implements BeerControllerApi {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public BeerDto createBeer(BeerDto beerDto) throws BeerAlreadyRegisteredException {
+  public BeerDto createBeer(@RequestBody @Valid BeerDto beerDto)
+      throws BeerAlreadyRegisteredException {
     return beerService.createBeer(beerDto);
   }
 
