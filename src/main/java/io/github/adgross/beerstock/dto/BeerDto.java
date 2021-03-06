96d39ff442e5d95b8a07459dfb211115d6cf1d34
Ádler Jonas Gross <adlerjgross@gmail.com>
2021-03-05 23:01:57 -0300

    BeerDto: add Min validation, so Swagger detect and show it

--- src/main/java/io/github/adgross/beerstock/dto/BeerDto.java
+++ src/main/java/io/github/adgross/beerstock/dto/BeerDto.java
@@ -4,6 +4,7 @@ import io.github.adgross.beerstock.enums.BeerType;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -34,11 +35,13 @@ public class BeerDto {
 
   @NotNull
   @Positive
+  @Min(1)
   @Max(500)
   private int max;
 
   @NotNull
   @PositiveOrZero
+  @Min(0)
   @Max(100)
   private int quantity;
 
