08995bbf868c6a320293efd1dc863357bf86de19
Ádler Jonas Gross <adlerjgross@gmail.com>
2021-03-09 00:15:25 -0300

    add builder support in Beer objects

--- src/main/java/io/github/adgross/beerstock/entity/Beer.java
+++ src/main/java/io/github/adgross/beerstock/entity/Beer.java
@@ -9,10 +9,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder(toBuilder = true)
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
