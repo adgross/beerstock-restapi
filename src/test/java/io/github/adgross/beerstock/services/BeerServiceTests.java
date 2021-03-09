d83cc24bf8e78dd530064b570030f844eb3cb99b
Ádler Jonas Gross <adlerjgross@gmail.com>
2021-03-09 03:05:30 -0300

    BeerServiceTests: implement more tests

--- src/test/java/io/github/adgross/beerstock/services/BeerServiceTests.java
+++ src/test/java/io/github/adgross/beerstock/services/BeerServiceTests.java
@@ -43,7 +43,7 @@ public class BeerServiceTests {
   private static final String NAME_INVALID = "invalid";
   private final Beer validBeer = new Beer(
       1L, "name", "brand", 400, 100, BeerType.FIRKANT);
-  private BeerMapper beerMapper = BeerMapper.INSTANCE;
+  private final BeerMapper beerMapper = BeerMapper.INSTANCE;
 
   @Mock
   private BeerRepository beerRepository;
@@ -200,7 +200,14 @@ public class BeerServiceTests {
   // implementation might check or not if exist
   @MockitoSettings(strictness = Strictness.LENIENT)
   void deleteBeerGivenRegisteredName() {
+    var beer = validBeer.toBuilder().build();
 
+    when(beerRepository.findByName(NAME_VALID)).thenReturn(Optional.of(beer));
+    doNothing().when(beerRepository).deleteByName(NAME_VALID);
+
+    beerService.deleteBeer(NAME_VALID);
+    verify(beerRepository, atMostOnce()).findByName(NAME_VALID);
+    verify(beerRepository, times(1)).deleteByName(NAME_VALID);
   }
 
   @Test
@@ -216,18 +223,74 @@ public class BeerServiceTests {
   }
 
   @Test
-  void updateBeerGivenRegisteredId() {
+  void updateBeerGivenRegisteredId()
+      throws BeerNotFoundException, BeerStockExceededException, BeerAlreadyRegisteredException {
+    var beer = validBeer.toBuilder().id(null).build();
+    var savedBeer = beer.toBuilder().id(ID_VALID).build();
+    var foundBeer = savedBeer.toBuilder().build();
+    var inputBeerDto = beerMapper.toDto(beer);
 
+    when(beerRepository.findById(ID_VALID)).thenReturn(Optional.of(foundBeer));
+    when(beerRepository.save(any(Beer.class))).thenReturn(savedBeer);
+
+    var updatedBeer = beerService.updateBeer(ID_VALID, inputBeerDto);
+    assertThat(updatedBeer.getId(), is(equalTo(ID_VALID)));
+    assertThat(updatedBeer.getName(), is(equalTo(inputBeerDto.getName())));
+    assertThat(updatedBeer.getQuantity(), is(equalTo(inputBeerDto.getQuantity())));
+    assertThat(updatedBeer.getMax(), is(equalTo(inputBeerDto.getMax())));
+    verify(beerRepository, times(1)).findById(ID_VALID);
+    verify(beerRepository, times(1)).save(any(Beer.class));
+    verify(beerRepository, never()).findByName(any(String.class));
   }
+
+
+  @Test
+  void updateBeerGivenRegisteredIdChangingTheName()
+      throws BeerNotFoundException, BeerStockExceededException, BeerAlreadyRegisteredException {
+    var beer = validBeer.toBuilder().id(null).build();
+    var savedBeer = beer.toBuilder().id(ID_VALID).build();
+    var foundBeer = savedBeer.toBuilder().name(NAME_VALID + "old").build();
+    var inputBeerDto = beerMapper.toDto(beer);
+
+    when(beerRepository.findById(ID_VALID)).thenReturn(Optional.of(foundBeer));
+    when(beerRepository.findByName(inputBeerDto.getName())).thenReturn(Optional.empty());
+    when(beerRepository.save(any(Beer.class))).thenReturn(savedBeer);
+
+    var updatedBeer = beerService.updateBeer(ID_VALID, inputBeerDto);
+    assertThat(updatedBeer.getId(), is(equalTo(ID_VALID)));
+    assertThat(updatedBeer.getName(), is(equalTo(inputBeerDto.getName())));
+    assertThat(updatedBeer.getQuantity(), is(equalTo(inputBeerDto.getQuantity())));
+    assertThat(updatedBeer.getMax(), is(equalTo(inputBeerDto.getMax())));
+    verify(beerRepository, times(1)).findById(ID_VALID);
+    verify(beerRepository, times(1)).findByName(any(String.class));
+    verify(beerRepository, times(1)).save(any(Beer.class));
+  }
+
 
   @Test
   void updateBeerGivenNotRegisteredId() {
+    var beer = validBeer.toBuilder().id(null).build();
+    var inputBeerDto = beerMapper.toDto(beer);
 
+    when(beerRepository.findById(ID_INVALID)).thenReturn(Optional.empty());
+
+    assertThrows(BeerNotFoundException.class,
+        () -> beerService.updateBeer(ID_INVALID, inputBeerDto));
+    verify(beerRepository, times(1)).findById(ID_INVALID);
+    verify(beerRepository, never()).findByName(any(String.class));
+    verify(beerRepository, never()).save(any(Beer.class));
   }
 
   @Test
-  void updateBeerGivenQuantityBiggerThanMax(){
+  void updateBeerGivenQuantityBiggerThanMax() {
+    var beer = validBeer.toBuilder().id(null).max(50).quantity(200).build();
+    var inputBeerDto = beerMapper.toDto(beer);
 
+    assertThrows(BeerStockExceededException.class,
+        () -> beerService.updateBeer(ID_VALID, inputBeerDto));
+    verify(beerRepository, never()).findById(any(Long.class));
+    verify(beerRepository, never()).findByName(any(String.class));
+    verify(beerRepository, never()).save(any(Beer.class));
   }
 
   @Test
@@ -237,45 +300,109 @@ public class BeerServiceTests {
     var inputBeerDto = beerMapper.toDto(beer);
     var inputId = ID_VALID;
     var foundByIdBeer = validBeer.toBuilder().id(inputId).name(NAME_VALID + "old").build();
-    var sameNameBeer = validBeer.toBuilder().id(ID_VALID+1).name(duplicateName).build();
+    var sameNameBeer = validBeer.toBuilder().id(ID_VALID + 1).name(duplicateName).build();
 
     when(beerRepository.findById(inputId)).thenReturn(Optional.of(foundByIdBeer));
     when(beerRepository.findByName(duplicateName)).thenReturn(Optional.of(sameNameBeer));
 
-    assertThrows(BeerAlreadyRegisteredException.class, () -> beerService.updateBeer(inputId, inputBeerDto));
+    assertThrows(BeerAlreadyRegisteredException.class,
+        () -> beerService.updateBeer(inputId, inputBeerDto));
     verify(beerRepository, times(1)).findById(inputId);
     verify(beerRepository, times(1)).findByName(duplicateName);
     verify(beerRepository, never()).save(any(Beer.class));
   }
 
   @Test
-  void incrementGivenRegisteredId() {
+  void incrementGivenRegisteredId() throws BeerStockExceededException, BeerNotFoundException {
+    int inputQuantity = 10;
+    var beer = validBeer;
+    var oldQuantity = beer.getQuantity();
+    var savedBeer = beer.toBuilder().quantity(oldQuantity + inputQuantity).build();
 
+    when(beerRepository.findById(ID_VALID)).thenReturn(Optional.of(beer));
+    when(beerRepository.save(any(Beer.class))).thenReturn(savedBeer);
+
+    var incrementedBeer = beerService.increment(ID_VALID, inputQuantity);
+    assertThat(incrementedBeer.getId(), is(equalTo(ID_VALID)));
+    assertThat(incrementedBeer.getName(), is(equalTo(beer.getName())));
+    assertThat(incrementedBeer.getMax(), is(equalTo(beer.getMax())));
+    assertThat(incrementedBeer.getQuantity(), is(equalTo(oldQuantity + inputQuantity)));
+    verify(beerRepository, times(1)).findById(ID_VALID);
+    verify(beerRepository, times(1)).save(any(Beer.class));
   }
 
   @Test
   void incrementGivenNotRegisteredId() {
+    int inputQuantity = 10;
 
+    when(beerRepository.findById(ID_INVALID)).thenReturn(Optional.empty());
+
+    assertThrows(BeerNotFoundException.class,
+        () -> beerService.increment(ID_INVALID, inputQuantity));
+    verify(beerRepository, times(1)).findById(ID_INVALID);
+    verify(beerRepository, never()).save(any(Beer.class));
   }
 
   @Test
   void incrementThatExceeds() {
+    int inputQuantity = 2;
+    var beer = validBeer.toBuilder()
+        .max(Integer.MAX_VALUE)
+        .quantity(Integer.MAX_VALUE - 1).build();
 
+    when(beerRepository.findById(ID_VALID)).thenReturn(Optional.of(beer));
+
+    assertThrows(BeerStockExceededException.class,
+        () -> beerService.increment(ID_VALID, inputQuantity));
+    verify(beerRepository, times(1)).findById(ID_VALID);
+    verify(beerRepository, never()).save(any(Beer.class));
   }
 
   @Test
-  void decrementGivenRegisteredId() {
+  void decrementGivenRegisteredId()
+      throws BeerNotFoundException, BeerStockNonExistentQuantityException {
+    int inputQuantity = 10;
+    var beer = validBeer;
+    var oldQuantity = beer.getQuantity();
+    var savedBeer = beer.toBuilder().quantity(oldQuantity - inputQuantity).build();
 
+    when(beerRepository.findById(ID_VALID)).thenReturn(Optional.of(beer));
+    when(beerRepository.save(any(Beer.class))).thenReturn(savedBeer);
+
+    var decrementedBeer = beerService.decrement(ID_VALID, inputQuantity);
+    assertThat(decrementedBeer.getId(), is(equalTo(ID_VALID)));
+    assertThat(decrementedBeer.getName(), is(equalTo(beer.getName())));
+    assertThat(decrementedBeer.getMax(), is(equalTo(beer.getMax())));
+    assertThat(decrementedBeer.getQuantity(), is(equalTo(oldQuantity - inputQuantity)));
+    verify(beerRepository, times(1)).findById(ID_VALID);
+    verify(beerRepository, times(1)).save(any(Beer.class));
   }
 
   @Test
   void decrementGivenNotRegisteredId() {
+    int inputQuantity = 10;
 
+    when(beerRepository.findById(ID_INVALID)).thenReturn(Optional.empty());
+
+    assertThrows(BeerNotFoundException.class,
+        () -> beerService.decrement(ID_INVALID, inputQuantity));
+    verify(beerRepository, times(1)).findById(ID_INVALID);
+    verify(beerRepository, never()).save(any(Beer.class));
   }
 
   @Test
   void decrementThatCauseNonExistentQuantity() {
+    int inputQuantity = Integer.MAX_VALUE;
+    var beer = validBeer.toBuilder()
+        .max(Integer.MAX_VALUE - 1)
+        .quantity(Integer.MAX_VALUE - 1).build();
 
+    when(beerRepository.findById(ID_VALID)).thenReturn(Optional.of(beer));
+
+    assertThrows(BeerStockNonExistentQuantityException.class,
+        () -> beerService.decrement(ID_VALID, inputQuantity));
+    verify(beerRepository, times(1)).findById(ID_VALID);
+    verify(beerRepository, never()).save(any(Beer.class));
   }
 
 }
