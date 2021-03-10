82daf0a2fd0813dc0d4970b6871bc8e53957d3bf
Ádler Jonas Gross <adlerjgross@gmail.com>
2021-03-10 15:34:39 -0300

    Tests: Don't include null values in json request

--- src/test/java/io/github/adgross/beerstock/utils/JsonConvertUtils.java
+++ src/test/java/io/github/adgross/beerstock/utils/JsonConvertUtils.java
@@ -1,12 +1,15 @@
 package io.github.adgross.beerstock.utils;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonConvertUtils {
 
   public static String asJsonString(final Object obj) {
     try {
-      return new ObjectMapper().writeValueAsString(obj);
+      return new ObjectMapper()
+          .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+          .writeValueAsString(obj);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
